761474abef874ca89786a58d3e20ecad1474716b
Lucas Souza <lucasrsouzasilva@gmail.com>
2024-01-23 21:55:43 +0000

    teste permission

---
+++ app/Providers/TEsteServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\ServiceProvider;
+
+class TEsteServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // $proxy_url = config('route.proxy_url');
+        // $proxy_scheme = config('route.proxy_scheme');
+
+        // if (!empty($proxy_url)) {
+        //     URL::forceRootUrl($proxy_url);
+        // }
+
+        // if (!empty($proxy_scheme)) {
+        //     URL::forceScheme($proxy_scheme);
+        // }
+    }
+}
